78e395752583742c1654d6b1771488a9d4436c5e
George Achkasov <georgij-net@yandex.ru>
2023-12-25 05:02:49 +0400

    format code

--- README.md
+++ README.md
@@ -8,19 +8,19 @@ This is a simple implementation of [Conway's Game of Life](https://en.wikipedia.
 
 The rules of the game are as follows:
 
-- Any live cell with two or three live neighbors lives on to the next generation.
-- Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
-- All others live cells die in the next generation.
-- Similarly, all others dead cells stay dead.
+-   Any live cell with two or three live neighbors lives on to the next generation.
+-   Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+-   All others live cells die in the next generation.
+-   Similarly, all others dead cells stay dead.
 
 For more detailed information, refer to the [Wikipedia page](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
 
 ## Features
 
-- The game board is implemented as a torus, where the rightmost cell is a neighbor of the leftmost cell with the same Y coordinate, and the top cell is a neighbor of the bottom cell with the same X coordinate.
-- The size of the game board can be dynamically adjusted through the graphical interface.
-- Initial generation can be generated either by user input using the mouse or randomly.
-- The time taken to generate a new generation is displayed on the screen.
+-   The game board is implemented as a torus, where the rightmost cell is a neighbor of the leftmost cell with the same Y coordinate, and the top cell is a neighbor of the bottom cell with the same X coordinate.
+-   The size of the game board can be dynamically adjusted through the graphical interface.
+-   Initial generation can be generated either by user input using the mouse or randomly.
+-   The time taken to generate a new generation is displayed on the screen.
 
 ## Implementation
 
--- js/main.js
+++ js/main.js
@@ -1,151 +1,151 @@
 function copy2dArray(array) {
-	return array.map((arr) => arr.slice());
+    return array.map((arr) => arr.slice());
 }
 
 class GameOfLife {
-	time = null;
-	timeoutId = null;
+    time = null;
+    timeoutId = null;
 
-	constructor({ root, w, h, fps }) {
-		if (!root) throw Error("Root not defined");
+    constructor({ root, w, h, fps }) {
+        if (!root) throw Error("Root not defined");
 
-		this.root = root;
-		this.w = Number(w) || 30;
-		this.h = Number(h) || 60;
-		this.fps = fps || 25;
+        this.root = root;
+        this.w = Number(w) || 30;
+        this.h = Number(h) || 60;
+        this.fps = fps || 25;
 
-		this.board = [];
-		this.elements = [];
-		this.isRunning = false;
-		this.directions = [
-			[-1, -1],
-			[-1, 0],
-			[-1, 1],
-			[0, 1],
-			[1, 1],
-			[1, 0],
-			[1, -1],
-			[0, -1],
-		];
+        this.board = [];
+        this.elements = [];
+        this.isRunning = false;
+        this.directions = [
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, 1],
+            [1, 1],
+            [1, 0],
+            [1, -1],
+            [0, -1],
+        ];
 
-		this.initialize();
-	}
+        this.initialize();
+    }
 
-	initialize() {
+    initialize() {
         // this.root.addEventListener('click', (evt) => {
         //     const target = evt.target;
         //     if (target.classList.contains('cell')) {
         //         // TODO
         //     }
         // })
-		this.prepareBoard();
-		this.randomizeBoard();
+        this.prepareBoard();
+        this.randomizeBoard();
         this.renderFrame();
-		this.updateUI();
-	}
+        this.updateUI();
+    }
 
-	prepareBoard() {
-		for (let i = 0; i < this.h; i++) {
-			this.board[i] = [];
-			this.elements[i] = [];
-			const row = document.createElement("div");
-			row.classList.add("row");
-			this.root.appendChild(row);
-			for (let j = 0; j < this.w; j++) {
-				this.board[i][j] = 0;
-				const cell = document.createElement("span");
-				cell.classList.add("cell");
-				row.appendChild(cell);
-				this.elements[i][j] = cell;
+    prepareBoard() {
+        for (let i = 0; i < this.h; i++) {
+            this.board[i] = [];
+            this.elements[i] = [];
+            const row = document.createElement("div");
+            row.classList.add("row");
+            this.root.appendChild(row);
+            for (let j = 0; j < this.w; j++) {
+                this.board[i][j] = 0;
+                const cell = document.createElement("span");
+                cell.classList.add("cell");
+                row.appendChild(cell);
+                this.elements[i][j] = cell;
 
-				cell.addEventListener("click", () => {
-					if (this.isRunning) return;
-					this.board[i][j] = this.board[i][j] === 1 ? 0 : 1;
+                cell.addEventListener("click", () => {
+                    if (this.isRunning) return;
+                    this.board[i][j] = this.board[i][j] === 1 ? 0 : 1;
                     if (this.board[i][j] === 1) {
                         cell.classList.add("alive");
                     } else {
                         cell.classList.remove("alive");
                     }
-				});
-			}
-		}
-	}
+                });
+            }
+        }
+    }
 
-	for(cb) {
-		if (!cb) return;
-		for (let i = 0; i < this.board.length; i++) {
-			for (let j = 0; j < this.board[i].length; j++) {
-				cb(this.board[i][j], this.elements[i][j], i, j);
-			}
-		}
-	}
+    for(cb) {
+        if (!cb) return;
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < this.board[i].length; j++) {
+                cb(this.board[i][j], this.elements[i][j], i, j);
+            }
+        }
+    }
 
-	randomizeBoard() {
-		this.for((_, __, i, j) => {
-			this.board[i][j] = Math.round(Math.random());
-		});
+    randomizeBoard() {
+        this.for((_, __, i, j) => {
+            this.board[i][j] = Math.round(Math.random());
+        });
         this.renderFrame();
-	}
+    }
 
-	clearBoard() {
-		this.for((_, __, i, j) => {
-			this.board[i][j] = 0;
-		});
+    clearBoard() {
+        this.for((_, __, i, j) => {
+            this.board[i][j] = 0;
+        });
         this.renderFrame();
-	}
+    }
 
-	tickNextGen() {
-		const clone = copy2dArray(this.board);
+    tickNextGen() {
+        const clone = copy2dArray(this.board);
 
-		this.for((cell, __, i, j) => {
-			const count = this.calcNeighbors(i, j);
-			if (cell === 1 && (count === 2 || count === 3)) return;
-			if (cell === 0 && count === 3) {
-				clone[i][j] = 1;
-				return;
-			}
-			clone[i][j] = 0;
-		});
+        this.for((cell, __, i, j) => {
+            const count = this.calcNeighbors(i, j);
+            if (cell === 1 && (count === 2 || count === 3)) return;
+            if (cell === 0 && count === 3) {
+                clone[i][j] = 1;
+                return;
+            }
+            clone[i][j] = 0;
+        });
 
-		this.board = clone;
-	}
+        this.board = clone;
+    }
 
-	calcNeighbors(i, j) {
-		let count = 0;
-		this.directions.forEach(([x, y]) => {
-			let ni = i + y;
-			let nj = j + x;
+    calcNeighbors(i, j) {
+        let count = 0;
+        this.directions.forEach(([x, y]) => {
+            let ni = i + y;
+            let nj = j + x;
 
-			if (ni < 0) {
-				ni = this.board.length + ni;
-			} else if (ni >= this.board.length) {
-				ni -= this.board.length;
-			}
+            if (ni < 0) {
+                ni = this.board.length + ni;
+            } else if (ni >= this.board.length) {
+                ni -= this.board.length;
+            }
 
-			if (nj < 0) {
-				nj = this.board[ni].length + nj;
-			} else if (nj >= this.board[ni].length) {
-				nj -= this.board[ni].length;
-			}
+            if (nj < 0) {
+                nj = this.board[ni].length + nj;
+            } else if (nj >= this.board[ni].length) {
+                nj -= this.board[ni].length;
+            }
 
-			if (this.board[ni][nj]) {
-				count++;
-			}
-		});
+            if (this.board[ni][nj]) {
+                count++;
+            }
+        });
 
-		return count;
-	}
+        return count;
+    }
 
-	tick() {
-		this.tickNextGen();
-		this.for((cell, ui) => {
-			if (cell === 1) {
-				ui.classList.add("alive");
-			} else {
-				ui.classList.remove("alive");
-			}
-		});
-	}
+    tick() {
+        this.tickNextGen();
+        this.for((cell, ui) => {
+            if (cell === 1) {
+                ui.classList.add("alive");
+            } else {
+                ui.classList.remove("alive");
+            }
+        });
+    }
 
     renderFrame() {
         const t0 = performance.now();
@@ -165,88 +165,88 @@ class GameOfLife {
         if (this.timeCb) this.timeCb(this.time);
     }
 
-	render() {
-		if (this.controller.signal.aborted) return;
+    render() {
+        if (this.controller.signal.aborted) return;
 
         this.renderFrame();
 
         this.timeoutId = setTimeout(
-			() => requestAnimationFrame(() => this.render()),
-			1000 / this.fps,
-		);
-	}
+            () => requestAnimationFrame(() => this.render()),
+            1000 / this.fps,
+        );
+    }
 
-	start() {
-		if (this.isRunning) return;
-		this.isRunning = true;
-		this.controller = new AbortController();
-		this.render();
-		this.updateUI();
-		if (this.startCb) this.startCb();
-	}
+    start() {
+        if (this.isRunning) return;
+        this.isRunning = true;
+        this.controller = new AbortController();
+        this.render();
+        this.updateUI();
+        if (this.startCb) this.startCb();
+    }
 
-	stop() {
-		this.isRunning = false;
-		this.updateUI();
-		this.controller.abort();
-		if (this.stopCb) this.stopCb();
-	}
+    stop() {
+        this.isRunning = false;
+        this.updateUI();
+        this.controller.abort();
+        if (this.stopCb) this.stopCb();
+    }
 
-	updateUI() {
-		if (this.isRunning) {
-			this.root.classList.add("active");
-			this.root.classList.remove("idle");
-		} else {
-			this.root.classList.add("idle");
-			this.root.classList.remove("active");
-		}
-	}
+    updateUI() {
+        if (this.isRunning) {
+            this.root.classList.add("active");
+            this.root.classList.remove("idle");
+        } else {
+            this.root.classList.add("idle");
+            this.root.classList.remove("active");
+        }
+    }
 
-	resizeBoard({ w = this.w, h = this.h }) {
-		const clearAndFillBoard = () => {
-			this.for((_, ui) => {
-				ui.remove();
-			});
-			this.elements = [];
-			this.board = [];
-			while (this.root.firstChild) {
-				this.root.firstChild.remove();
-			}
-			this.w = Number(w);
-			this.h = Number(h);
-			this.prepareBoard();
-			this.randomizeBoard();
-		};
+    resizeBoard({ w = this.w, h = this.h }) {
+        const clearAndFillBoard = () => {
+            this.for((_, ui) => {
+                ui.remove();
+            });
+            this.elements = [];
+            this.board = [];
+            while (this.root.firstChild) {
+                this.root.firstChild.remove();
+            }
+            this.w = Number(w);
+            this.h = Number(h);
+            this.prepareBoard();
+            this.randomizeBoard();
+        };
 
-		if (this.isRunning) {
-			this.controller.abort();
-			clearTimeout(this.timeoutId);
-			clearAndFillBoard();
-			this.controller = new AbortController();
-			this.render();
-		} else {
-			clearAndFillBoard();
-			this.for((cell, ui) => {
-				if (cell === 1) {
-					ui.classList.add("alive");
-				} else {
-					ui.classList.remove("alive");
-				}
-			});
-		}
-	}
+        if (this.isRunning) {
+            this.controller.abort();
+            clearTimeout(this.timeoutId);
+            clearAndFillBoard();
+            this.controller = new AbortController();
+            this.render();
+        } else {
+            clearAndFillBoard();
+            this.for((cell, ui) => {
+                if (cell === 1) {
+                    ui.classList.add("alive");
+                } else {
+                    ui.classList.remove("alive");
+                }
+            });
+        }
+    }
 
-	onStop(cb) {
-		this.stopCb = cb;
-	}
+    onStop(cb) {
+        this.stopCb = cb;
+    }
 
-	onStart(cb) {
-		this.startCb = cb;
-	}
+    onStart(cb) {
+        this.startCb = cb;
+    }
 
-	onTime(cb) {
-		this.timeCb = cb;
-	}
+    onTime(cb) {
+        this.timeCb = cb;
+    }
 }
 
 const root = document.getElementById("game");
@@ -262,9 +262,9 @@ height.value = 60;
 const game = new GameOfLife({ root, w: width.value, h: height.value });
 
 const addListener = (id, eventType, cb) => {
-	const element = document.getElementById(id);
-	if (!element) throw Error(`Element '#${id}' not find`);
-	element.addEventListener(eventType, cb);
+    const element = document.getElementById(id);
+    if (!element) throw Error(`Element '#${id}' not find`);
+    element.addEventListener(eventType, cb);
 };
 
 addListener("start", "click", () => game.start());
@@ -273,29 +273,29 @@ addListener("tick", "click", () => game.tick());
 addListener("random", "click", () => game.randomizeBoard());
 addListener("clear", "click", () => game.clearBoard());
 addListener("width", "change", (event) => {
-	game.resizeBoard({ w: event.currentTarget.value });
+    game.resizeBoard({ w: event.currentTarget.value });
 });
 addListener("height", "change", (event) => {
-	game.resizeBoard({ h: event.currentTarget.value });
+    game.resizeBoard({ h: event.currentTarget.value });
 });
 
 stop.classList.add("disabled");
 stop.disabled = true;
 
 game.onStart(() => {
-	start.classList.add("disabled");
-	start.disabled = true;
-	stop.classList.remove("disabled");
-	stop.disabled = false;
+    start.classList.add("disabled");
+    start.disabled = true;
+    stop.classList.remove("disabled");
+    stop.disabled = false;
 });
 
 game.onStop(() => {
-	start.classList.remove("disabled");
-	start.disabled = false;
-	stop.classList.add("disabled");
-	stop.disabled = true;
+    start.classList.remove("disabled");
+    start.disabled = false;
+    stop.classList.add("disabled");
+    stop.disabled = true;
 });
 
 game.onTime((ms) => {
-	time.innerHTML = `${ms.toFixed(5)}ms`;
+    time.innerHTML = `${ms.toFixed(5)}ms`;
 });
